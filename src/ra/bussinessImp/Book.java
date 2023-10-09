ba8dfcee7af8e7ca23e7789b00199f6f5898c86b
unknown <nshoang270298@gmail.com>
2023-10-09 10:48:28 +0700

    hackathon 10-9-23

---
+++ src/ra/bussinessImp/Book.java
@@ -0,0 +1,196 @@
+package ra.bussinessImp;
+
+import ra.bussiness.IBook;
+
+import static ra.run.BookManagement.* ;
+
+public class Book implements IBook {
+    private static int nextId = 1 ;
+    private int bookId ;
+    private String bookName ;
+    private String title;
+    private int numberOfPages ;
+    private float importPrice ;
+    private float exportPrice ;
+    private float interest ;
+    private boolean bookStatus ;
+    public Book() {
+        this.bookId = nextId++ ;
+    }
+
+    public Book(int bookId, String bookName, String title, int numberOfPages, float importPrice, float exportPrice, float interest, boolean bookStatus) {
+        this.bookId = bookId;
+        this.bookName = bookName;
+        this.title = title;
+        this.numberOfPages = numberOfPages;
+        this.importPrice = importPrice;
+        this.exportPrice = exportPrice;
+        this.interest = interest;
+        this.bookStatus = bookStatus;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public static void setNextId(int nextId) {
+        Book.nextId = nextId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public float getImportPrice() {
+        return importPrice;
+    }
+
+    public void setImportPrice(float importPrice) {
+        this.importPrice = importPrice;
+    }
+
+    public float getExportPrice() {
+        return exportPrice;
+    }
+
+    public void setExportPrice(float exportPrice) {
+        this.exportPrice = exportPrice;
+    }
+
+    public float getInterest() {
+        return interest;
+    }
+
+    public void setInterest(float interest) {
+        this.interest = interest;
+    }
+
+    public boolean isBookStatus() {
+        return bookStatus;
+    }
+
+    public void setBookStatus(boolean bookStatus) {
+        this.bookStatus = bookStatus;
+    }
+
+    @Override
+    public void inputData() {
+        // ten sach
+        while (true) {
+            System.out.println("Nhập vào tên sách ");
+            String name = sc.nextLine();
+            if (name.trim().isEmpty()) {
+                System.err.println("Ten sach khong the de rong. Vui long nhap lai ❤");
+            } else {
+                this.bookName = name ;
+                break;
+            }
+        }
+
+        // tieu de
+        while (true) {
+            System.out.println("Nhap vao tieu de sach");
+            String tieude = sc.nextLine();
+            if (tieude.trim().isEmpty()) {
+                System.err.println("Tieu de sach khong dc de rong. Vui long nhap lai ❤");
+            } else {
+                this.title = tieude;
+                break;
+            }
+        }
+
+        // so trang sach
+       while (true) {
+           System.out.println("Nhap vao so trang sach");
+           int number = Integer.parseInt(sc.nextLine()) ;
+           if (number <= 0 ) {
+               System.err.println("So trang sach khong the nho hon 0 . Vui long nhap lai ❤");
+           } else {
+               this.numberOfPages = number ;
+               break;
+           }
+       }
+
+        // gia nhap vao
+        while (true) {
+            System.out.println("Gia nhap vao ");
+            float priceImport = Float.parseFloat(sc.nextLine());
+            if ( priceImport <= 0 ) {
+                System.err.println("Gia nhap vao khong the nho hon 0 . Vui long nhap lai ❤");
+            } else {
+                this.importPrice = priceImport ;
+                break;
+            }
+        }
+
+        // gia ban ra
+        while (true) {
+            System.out.println("Gia ban ra ");
+            float priceExport = Float.parseFloat(sc.nextLine()) ;
+            if (priceExport <= this.importPrice) {
+                System.err.println("Gia ban ra khong the nho hon gia nhap vao . Vui long nhap lai ❤");
+            } else {
+                this.exportPrice = priceExport ;
+                break;
+            }
+        }
+
+        while (true) {
+            System.out.println("Trang thai ban hang : ( Co - Khong ) ");
+            String trangthai = sc.nextLine();
+            if (trangthai.equalsIgnoreCase("co")) {
+                this.bookStatus = true ;
+                break;
+            } else if (trangthai.equalsIgnoreCase("khong")) {
+                this.bookStatus = false ;
+                break;
+            } else {
+                System.err.println("Trang thai ban hang khong dung dinh dang. Vui long nhap lai ❤");
+            }
+        }
+        this.interest = this.exportPrice - this.importPrice ;
+
+    }
+
+    @Override
+    public void displayData() {
+        System.out.println("___________ THONG TIN SACH ____________");
+        System.out.println("Id : " + bookId);
+        System.out.println("Ten sach : " + bookName);
+        System.out.println("Tieu de : " + title);
+        System.out.println("So trang sasch : " + numberOfPages);
+        System.out.println("Gia nhap vao : " + importPrice);
+        System.out.println("Gia ban ra : " + exportPrice);
+        System.out.println("Loi nhuan : " + interest);
+        System.out.println("Trang thai ban hang : " + (bookStatus?"Co ban":"Khong ban"));
+
+    }
+}
---
+++ src/ra/run/BookManagement.java
@@ -0,0 +1,159 @@
+package ra.run;
+
+import ra.bussinessImp.Book;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class BookManagement {
+    public static Scanner sc = new Scanner(System.in) ;
+    public static List<Book> listBook = new ArrayList<>();
+
+    public static void main(String[] args) {
+        BookManagement bookManagement = new BookManagement();
+
+//        Book book1 = new Book(1 , "Truyen kieu", "ccc" , 100, 50F, 100F, true);
+//        Book book2 = new Book(2 , "Chi pheo", "ccc" , 100, 60F, 120F, true);
+//        listBook.add(book1);
+//        listBook.add(book2);
+        int choice ;
+        do {
+            System.out.println("_______________JAVA-HACKATHON-05-BASIC-MENU________________");
+            System.out.println("1. Nhập số sách và nhập thông tin sách ");
+            System.out.println("2. Hiển thị thông tin các sách");
+            System.out.println("3. Sắp xếp sách theo lợi nhuận giảm dần");
+            System.out.println("4. Xóa sách theo mã sách");
+            System.out.println("5. Tìm kiếm sách theo tên sách");
+            System.out.println("6. Thay đổi trạng thái của sách theo mã sách");
+            System.out.println("7. Thoát");
+            System.out.println("Lua chon the thua hien quan ly MENU Book ");
+            choice = Integer.parseInt(sc.nextLine()) ;
+
+            switch (choice) {
+                case 1 :
+                    bookManagement.createBook();
+                    break;
+                case 2 :
+                    bookManagement.printBook();
+                    break;
+                case 3 :
+                    bookManagement.sortByPrice();
+                    break;
+                case 4 :
+                    bookManagement.deleteById();
+                    break;
+                case 5 :
+                    bookManagement.findByName();
+                    break;
+                case 6 :
+                    bookManagement.changeStatus();
+                    break;
+                case 7 :
+                    System.err.println(" ❌❌❌ Thoat chuong trinh ");
+                    sc.close();
+                    break;
+                default:
+                    System.err.println("Lua chon khong phu hop . Vui long nhap lai ❤");
+            }
+
+        } while (choice!= 7) ;
+    }
+
+    public void createBook() {
+        System.out.println("Nhap vao so luong sach muon them vao danh sach ");
+        int n = Integer.parseInt(sc.nextLine()) ;
+        for (int i = 0; i < n; i++) {
+            Book newBook = new Book();
+            newBook.inputData();
+            listBook.add(newBook) ;
+        }
+    }
+
+    public void printBook() {
+        for ( Book book: listBook) {
+            if (listBook.isEmpty()) {
+                System.out.println("Danh sach dang rong , Hay nhap vao sach them vao danh sach");
+                Book newBook = new Book();
+                newBook.inputData();
+                listBook.add(newBook) ;
+                listBook.get(0).displayData();
+            } else {
+                book.displayData();
+            }
+        }
+    }
+
+    public void sortByPrice() {
+        listBook.sort(new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return (int) (o2.getInterest() - o1.getInterest());
+            }
+        });
+        System.out.println("Sap xep theo loi nhuan giam dan ");
+       for (Book book : listBook) {
+           book.displayData();
+       }
+    }
+
+    public void deleteById() {
+        System.out.println("Nhap vao Id sach can xoa");
+        int id = Integer.parseInt(sc.nextLine()) ;
+        Book newBook = findById(id) ;
+        if ( newBook != null) {
+            listBook.remove(newBook);
+            System.out.println("Da xoa sach khoi danh sach");
+        } else {
+            System.err.println("Khong tim thay sach trnong danh sach ");
+        }
+    }
+
+    public void findByName() {
+        System.out.println("Nhap vao ten sach can tim kiem ");
+        String search = sc.nextLine() ;
+        for (Book book : listBook) {
+            if ( book.getBookName().equalsIgnoreCase(search)) {
+                System.out.println("Tim thay sach trong danh sach");
+                book.displayData();
+                break;
+            } else {
+                System.err.println("Sach khon ton tai ");
+                break;
+            }
+        }
+    }
+
+    public void changeStatus() {
+        System.out.println("Nhap vao Id sach can thay doi trang thai ban hang");
+        int id = Integer.parseInt(sc.nextLine()) ;
+        Book newBook = findById(id) ;
+        int index = listBook.indexOf(newBook) ;
+        if (newBook != null) {
+            System.out.println("Trang thai ban hang hien tai " + (newBook.isBookStatus()?"Co ban":"Khong ban"));
+          while (true) {
+              System.out.println("Thay doi trang thai ban hang ( Co - Khong ) ");
+              String newStatus = sc.nextLine();
+              if (newStatus.equalsIgnoreCase("co")) {
+               newBook.setBookStatus(true);
+               break;
+              } else if (newStatus.equalsIgnoreCase("khong")) {
+                  newBook.setBookStatus(false);
+                  break;
+              } else {
+                  System.err.println("Trang thai ban hang khong phu hop . Vui long nhap lai ❤");
+              }
+          }
+        }
+    }
+
+    public Book findById(int id ) {
+        for (Book book : listBook) {
+            if (book.getBookId() == id ) {
+                return book ;
+            }
+        }
+        return null ;
+    }
+}
